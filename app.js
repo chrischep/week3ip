40f6b71dfa0ce212ee4acbd76874ca13c855e0dc
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-17 13:17:10 +0300

    add sumData

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ function produceReport(){
     data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
     data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
     data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
-
+    
+    let sumData = data.shed_A + data.shed_B + data.shed_C + data.shed_D;
 }
