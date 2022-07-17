f6eb605d0a20ec22f9d494d2d61dadfb438d3050
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-17 13:10:38 +0300

    add shed data array

---
+++ app.js
@@ -0,0 +1,10 @@
+function produceReport(){
+    event.preventDefault();
+    let data = [];
+
+    data[0] = data["shed_A"] = parseInt(document.getElementById("shed_A").value);
+    data[1] = data["shed_B"] = parseInt(document.getElementById("shed_B").value);
+    data[2] = data["shed_C"] = parseInt(document.getElementById("shed_C").value);
+    data[3] = data["shed_D"] = parseInt(document.getElementById("shed_D").value);
+
+}
