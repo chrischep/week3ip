4b440cd523a7efd8c288e9ba9a1d601a5fbcf2b2
chrischep <christine.chepleting@student.moringaschool.com>
2022-07-17 13:26:24 +0300

    add data for a leap year

--- app.js
+++ app.js
@@ -60,4 +60,16 @@ function produceReport(){
     document.getElementById("outputData").innerHTML += "<p> Your total income for October is Ksh. " + incomeOctober + " </p>"
     document.getElementById("outputData").innerHTML += "<p> Your total income for November is Ksh. " + incomeNovember + " </p>"
     document.getElementById("outputData").innerHTML += "<p> Your total income for December is Ksh. " + incomeDecember + " </p>"
+
+    document.getElementById("outputData").innerHTML += "<Br>"
+
+    document.getElementById("outputData").innerHTML += "<hr>"
+
+    incomeInLeapYear = dailyIncome * 366;
+
+    document.getElementById("outputData").innerHTML += "<p>Your total income in a leap year is Ksh. " + incomeInLeapYear + " </p>";
+
+    document.getElementById("outputData").innerHTML += "<hr>"
+
+    document.getElementById("outputData").innerHTML += "<Br>"
 }
